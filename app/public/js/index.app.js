2416c75a00185f0a9f1ebe594fc7af8817073c1c
matssmit <matssmit@iu.edu>
2020-09-18 06:47:48 -0400

    1) defined JSON values to be populated by RandomUser API
    2) inserted fetch method from Vue.JS documentation

--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -1,33 +1,31 @@
 var app = new Vue({
-  el: '#cardPaneLeft',
+  el: '#userProfile',
   data: {
-    message: 'Hello Vue!',
-    onePt: {
-      "patientGuid": "SOME-REALLY-LONG-1234",
-      "firstName": "Sylvia",
-      "lastName": "Hernandez",
-      "dob": "2012-09-01",
-      "sexAtBirth": "F",
-      "priority": "critical"
-    },
-    ptList: [],
-    user: {},
-    times: 0
+    userName: 'Matt',
+    userLast:'Smith',
+    userEmail: 'matssmit@iu.edu',
+    userCountry: 'United State',
+    userImage: '',
+    userImgThumb: '',
   },
-  methods: {
-    yell(msg) {
-      this.times = this.times + 1;
-      var msg = "Clicked " +this.times+ " times";
-      alert(msg);
-    }
-  },
-  created() {
-    fetch("dummy/pt-list.php")
-    .then( response => response.json() )
-    .then( json => {
-      this.ptList = json;
 
-      console.log(json)}
-    );
-  }
-})
+  created() {
+    this.fetchUser();
+  },
+
+  methods: {
+    fetchUser: function () {
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json ())
+      .then (data => {
+        var userData = data.results[0];
+        console.log(userData);
+        this.userName = userData.name.first;
+        this.userLast = userData.name.last;
+        this.userEmail = userData.email;
+        this.userCountry = userData.nat;
+        this.userImage = userData.picture.large;
+        this.userImgThumg = userData.picture.thumbnail;
+        })
+      }
+    }})
