91b27ecde1298e91cc52aa17aa28f6eaa92fc73d
matssmit <matssmit@iu.edu>
2020-10-20 13:13:29 -0400

    finished :)

--- app/public/js/comments.app.js
+++ app/public/js/comments.app.js
@@ -2,7 +2,10 @@ var app = new Vue({
     el: '#userComments',
     data: {
       comments: [],
-      commentForm: {}
+      commentList: [],
+      commentForm: {},
+      newCommentData: {},
+      newComment: {},
     },
     methods: {  
       getNewCommentForm() {
@@ -11,16 +14,36 @@ var app = new Vue({
         }
       },
       handleNewCommentForm( evt ) {
-       fetch("api/comments/create.php")
-       JSON.stringify(this.commentForm)
-      }
-    },
-    created(){
-      fetch("api/comments/")
-      .then(response => response.json())
-      .then(json => {
-        this.comments = json;
-        console.log(this.comments)}
-      );
-    }
-  })
+       fetch("api/comments/create.php", {
+        method: 'POST',
+        body: JSON.stringify(this.commentForm),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8" 
+       }
+    })
+      .then( response => response.json() )
+      .then( json => {
+        console.log ("Returned from post:", json);
+        this.commentList = json;
+        this.commentForm = this.getNewCommentForm();
+      });
+      console.log("Creating (POSTing)...!");
+      console.log("this.newComment");
+      },
+
+    // newCommentData() {
+    //   return {
+    //     commentText: ''        
+    //   }
+    // }
+  },
+  created() {
+    fetch("api/comments/")
+    .then( response => response.json() )
+    .then( json => {
+      this.commentList = json;
+      console.log(json)}
+    );}
+});
+
+  
